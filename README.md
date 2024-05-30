8cbbf577711d6183ee54576fb2ad421b523967c6
Abreham Mindaye <94402109+abe-min@users.noreply.github.com>
2024-05-30 18:38:55 -0400

    Added Wifi App  section of the program

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ESP32_IoT_App_Temp_Sensor
- An ESP32 based IoT app
+# Temperature Sensor IoT Application with ESP32
+
+### Summary
+A WiFi based application on the ESP32 and Subscribe/Publish MQTT Messages to AWS IoT using ESP-IDF & FreeRTOS
+
+### App Features 
+
+1. WIFI Application
+    - ESP32 starts an access point 
+        - This enables users to access info from the ESP32 SOC (ie senosrs, device info, connection status, etc..)
+    - The WIFI app will start an HTTP server which will support a web page to enable the above
+    - The app will contain a FreeRTOS task that accepts FreeRTOS Queue messages (xQUeueCreate, xQueueSend, and xQueueReceive)
+2. HTTP Server 
+3. OTA FW UpDATE
+4. DHT22 Sensor 
+5. Non-Volatile Storage 
+6. RGB LED
+7. SNTP
+8. Button with Interrupt and Semaphore 
+
--- main/wifi_app.c
+++ main/wifi_app.c
@@ -26,4 +26,83 @@ static const char TAG [] = "wifi_app";
 static QueueHandle_t wifi_app_queue_handle;
 
 // netif objects for the station access point
+esp_netif_t* esp_netif_sta = NULL;
+esp_netif_t* esp_netif_ap = NULL;
+
+/**
+ * Main task for WiFi application
+ * @param pvParameters parameter which can be passed to the task
+ */
+
+static void wifi_app_task(void *pvParameters)
+{
+	wifi_app_queue_message_t msg;
+
+	// Initialize event handler
+	wifi_app_event_handler_init();
+
+	// Initialize the TCP/IP stack and WiFI config
+	wifi_app_default_wifi_init();
+
+	// SoftAP config
+	wifi_app_soft_ap_config();
+
+	// Start WiFi
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	// Send first event message
+
+	wifi_app_send_message(WIFI_APP_MSG_START_HTTP_SERVER);
+
+	for (;;)
+	{
+		if (xQueueReceive(wifi_app_queue_handle, &msg, portMAX_DELAY))
+		{
+			switch (msg.msgID)
+			{
+				case WIFI_APP_MSG_START_HTTP_SERVER;
+					ESP_LOGIC(TAG, "WIFI_APP_MSG_START_HTTP_SERVER");
+					rgb_led_http_server_started();
+
+					break;
+
+				case WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER;
+					ESP_LOGIC(TAG, "WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER");
+
+					break;
+
+				case WIFI_APP_MSG_STA_CONNECTED_GOT_IP;
+					ESP_LOGIC(TAG, "WIFI_APP_MSG_STA_CONNECTED_GOT_IP");
+					rgb_led_wifi_connected();
+
+					break;
+
+				default:
+					break;
+
+			}
+		}
+	}
+}
+
+BaseType_t wifi_app_send_message(wifi_app_message_e msgID){
+	wifi_app_queue_message_t msg;
+	msg.msgID = msgID;
+	return xQueueSend(wifi_app_queue_handle, &msg, portMAX_DELAY);
+}
+
+void wifi_app_start(void){
+	ESP_LOG(TAG, "Starting WIFI APP");
+
+	// start WiFi started LED
+	rgb_led_wifi_app_started();
+
+	esp_log_level_set("wifi", ESP_LOG_NONE);
+
+	wifi_app_queue_handle = xQueueCreate(3, sizeof(wifi_app_queue_message_t));
+
+	// Start WiFi application task
+	xTaskCreatePinnedToCore(&wifi_app_task, "wifi_app_task", WIFI_APP_TASK_STACK_SIZE, NULL, WIFI_APP_TASK_PRIORITY, NULL, WIFI_APP_TASK_CORE_ID);
+}
+
 
